71226872276b901a5dac269b30137f09d9c9be49
matias-2 <mat.pacheco@duocuc.cl>
2024-05-07 22:12:23 -0400

    Add files via upload

---
+++ fpy1101-012v.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+
+while(true):
+    print ("1.pikachu roll %4500")
+    print ("2. otaku roll %5000")
+    print ("3. pulpo venenoso roll %5200")
+    print ("4. anguila electrica roll %4800")
+    try:
+        pedido=int(input ("ingrese una opcion del menu"))
+    if pedido>1 and pedido =<4:
+       if pedido==1
+       continue
+    os.system("cls")
+    
+    input ("posee un codigo de descuento")
+    input ("ingrese su codigo de descuento")
+    
